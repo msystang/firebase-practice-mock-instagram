312188457e052d85d0a51fe609789b5fdae598c3
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-11-19 14:37:57 -0500

    adds lazy UI objects to feedCollectionViewCell

--- firebase-practice-mock-instagram/firebase-practice-mock-instagram/Views/FeedCollectionViewCell.swift
+++ firebase-practice-mock-instagram/firebase-practice-mock-instagram/Views/FeedCollectionViewCell.swift
@@ -9,5 +9,14 @@
 import UIKit
 
 class FeedCollectionViewCell: UICollectionViewCell {
+    lazy var postImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    lazy var displayNameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
     
 }
