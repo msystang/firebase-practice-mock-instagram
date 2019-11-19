661ab866a2b78b5aa790da5818244cd89fe8fe09
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-11-19 13:30:23 -0500

    initial commit

---
+++ firebase-practice-mock-instagram/firebase-practice-mock-instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase-practice-mock-instagram
+//
+//  Created by Sunni Tang on 11/19/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
