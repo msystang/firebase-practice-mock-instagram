b077ca46932f955e9fa7607369f69d177a699c10
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-11-24 13:16:01 -0500

    creates Post model

--- firebase-practice-mock-instagram/firebase-practice-mock-instagram/Models/Post.swift
+++ firebase-practice-mock-instagram/firebase-practice-mock-instagram/Models/Post.swift
@@ -7,7 +7,36 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 struct Post {
+    let id: String
+    let creatorID: String
+    let photoUrl: String?
+    let dateCreated: Date?
     
+    init(creatorID: String, photoUrl: String? = nil, dateCreated: Date? = nil) {
+        self.id = UUID().description
+        self.creatorID = creatorID
+        self.photoUrl = photoUrl
+        self.dateCreated = dateCreated
+    }
+    
+    init?(from dict: [String: Any], id: String) {
+        guard let userID = dict["creatorID"] as? String,
+            let photoUrl = dict["photoUrl"] as? String else { return nil }
+            let dateCreated = (dict["dateCreated"] as? Timestamp)?.dateValue()
+        
+        self.id = id
+        self.creatorID = userID
+        self.photoUrl = photoUrl
+        self.dateCreated = dateCreated
+    }
+    
+    var fieldsDict: [String: Any] {
+        return [
+            "creatorID": self.creatorID,
+            "photoUrl": self.photoUrl
+        ]
+    }
 }
