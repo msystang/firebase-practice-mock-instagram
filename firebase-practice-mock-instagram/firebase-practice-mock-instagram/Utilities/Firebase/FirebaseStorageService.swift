219c99b32ad868aeddd7ac5e6f921e9b12ba37f5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-11-24 20:17:13 -0500

    adds FirebaseStorageService manager

---
+++ firebase-practice-mock-instagram/firebase-practice-mock-instagram/Utilities/Firebase/FirebaseStorageService.swift
@@ -0,0 +1,67 @@
+//
+//  File.swift
+//  firebase-practice-mock-instagram
+//
+//  Created by Sunni Tang on 11/24/19.
+//  Copyright © 2019 Sunni Tang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseStorage
+
+class FirebaseStorageService {
+    enum ImageType {
+        case profileImage
+        case post
+    }
+    
+    static var profileImageManager = FirebaseStorageService(type: .profileImage)
+    static var postManager = FirebaseStorageService(type: .post)
+    
+    private let storage: Storage!
+    private let storageReference: StorageReference
+    private let imagesFolderReference: StorageReference
+    
+    init(type: ImageType) {
+        storage = Storage.storage()
+        storageReference = storage.reference()
+        
+        switch type {
+        case .profileImage:
+            imagesFolderReference = storageReference.child("profileImages")
+        case .post:
+            imagesFolderReference = storageReference.child("posts")
+        }
+        
+    }
+    
+    func storeImage(image: Data,  completion: @escaping (Result<String,Error>) -> ()) {
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        let uuid = UUID()
+        let imageLocation = imagesFolderReference.child(uuid.description)
+        imageLocation.putData(image, metadata: metadata) { (responseMetadata, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                imageLocation.downloadURL { (url, error) in
+                    guard error == nil else {completion(.failure(error!));return}
+                    guard let url = url?.absoluteString else {completion(.failure(error!));return}
+                    completion(.success(url))
+                }
+            }
+        }
+    }
+    
+    func getImage(photoUrl: URL, completion: @escaping (Result<UIImage,Error>) -> ()) {
+        imagesFolderReference.storage.reference(forURL: photoUrl.absoluteString).getData(maxSize: 20000) { (data, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let image = UIImage(data: data) {
+                completion(.success(image))
+            }
+        }
+    }
+    
+}
